14f2058362204d651ec36634664741bc449a304a
Bettyrosew <bettyrosaw@gmail.com>
2021-08-09 02:45:59 +0300

    updates user profile

--- TransportMainApp-authenticationmodule/app/src/main/java/com/example/transportmainapp/ProfileActivity.java
+++ TransportMainApp-authenticationmodule/app/src/main/java/com/example/transportmainapp/ProfileActivity.java
@@ -30,7 +30,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 startActivity(new Intent(ProfileActivity.this, MainActivity.class));
                 break;
             case R.id.driver_button_id:
-                startActivity(new Intent(ProfileActivity.this, DriverActivity.class));
+                startActivity(new Intent(ProfileActivity.this, EditProfilePage.class));
                 break;
             case R.id.passenger_button_id:
                 startActivity(new Intent(ProfileActivity.this, Passenger_Activity.class));
